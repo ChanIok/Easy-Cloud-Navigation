a8aabfac81f18f1aae6aaabe8cb474df55079a3e
ChanIok <654232825@qq.com>
2021-07-27 18:05:51 +0800

    chore: remove redis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,21 +5,21 @@ import { DbModule } from './db/db.module';
 
 import { Log4jsModule } from '@nestx-log4js/core';
 import { AuthModule } from './auth/auth.module';
-import { RedisModule, RedisModuleOptions } from 'nestjs-redis';
+// import { RedisModule, RedisModuleOptions } from 'nestjs-redis';
 import { FavorModule } from './modules/favor/favor.module';
 
-const options: RedisModuleOptions = {
-  name: 'management',
-  port: 6379,
-  host: '127.0.0.1',
-};
+// const options: RedisModuleOptions = {
+//   name: 'management',
+//   port: 6379,
+//   host: '127.0.0.1',
+// };
 
 @Module({
   imports: [
     DbModule,
     Log4jsModule.forRoot(),
     AuthModule,
-    RedisModule.register(options),
+    // RedisModule.register(options),
     FavorModule,
   ],
   controllers: [AppController],
